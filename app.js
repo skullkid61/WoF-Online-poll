eccd8f709370e9ac0b2f82b483907a295926d9b6
skullkid61 <43558378+skullkid61@users.noreply.github.com>
2019-07-03 22:11:02 +0930

    Moved form submission button

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ nominees.addEventListener('click', e => {
 			console.log('This click works for nominee ' + (i+1));
 			hiddenRadio[i].click();
 			e.target.classList.add('border-focus');
+		} else if (e.target === nominees) {
+
 		} else {
 			card[i].classList.remove('border-focus');
 		}
