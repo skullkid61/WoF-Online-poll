324a8bce8b686f5823e9ecc504fa4976ede662cd
Dace Howard <dace61@gmail.com>
2019-07-03 10:19:18 +0930

    Radio click working

---
+++ app.js
@@ -0,0 +1,12 @@
+const hiddenRadio = document.querySelectorAll('input[type=radio]');
+const nominees = document.getElementById('nominees');
+const card = document.querySelectorAll('.nominee-card');
+
+nominees.addEventListener('click', e => {
+	for (i = 0; i < card.length; i++) {
+		if (e.target === card[i]) {
+			console.log('This click works for nominee ' + (i+1));
+			hiddenRadio[i].click();
+		}
+	}
+});
