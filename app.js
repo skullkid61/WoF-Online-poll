6e2f5c160b2416ac24904b50214b435ac98eb609
Dace Howard <dace61@gmail.com>
2019-07-04 15:12:44 +0930

    Added extra styling to nominee selection

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const hiddenRadio = document.querySelectorAll('input[type=radio]');
 const nominees = document.getElementById('nominees');
 const card = document.querySelectorAll('.nominee-card');
+const submit = document.getElementById('vote-now');
+const nomineeName = document.querySelectorAll('.card-text h2');
 
 nominees.addEventListener('click', e => {
 	for (i = 0; i < card.length; i++) {
 		if (e.target === card[i]) {
 			console.log('This click works for nominee ' + (i+1));
-			hiddenRadio[i].click();
-			e.target.classList.add('border-focus');
-		} else if (e.target === nominees) {
-
+			hiddenRadio[i].checked = true;
+      e.target.classList.add('border-focus');
+      submit.style.display = 'block';
+      submit.setAttribute('value', 'Vote for ' + nomineeName[i].innerText);
 		} else {
 			card[i].classList.remove('border-focus');
+      hiddenRadio[i].checked = false;
 		}
 	}
 });
