828cf80204e8320ae1aa57fabd9a6a844b3182cd
Dace Howard <dace61@gmail.com>
2019-07-07 11:25:58 +0930

    Added individual buttons

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const nominees = document.getElementById('nominees');
 const card = document.querySelectorAll('.nominee-card');
 const submit = document.getElementById('vote-now');
 const nomineeName = document.querySelectorAll('.card-text h2');
+const voteButton = document.querySelectorAll('.single-vote');
 
 document.addEventListener('click', (e) => {
   let count = 0;
@@ -11,13 +12,17 @@ document.addEventListener('click', (e) => {
       console.log('This click works for nominee ' + (i+1));
 			hiddenRadio[i].checked = true;
       e.target.classList.add('border-focus');
+      voteButton[i].classList.remove('hide-vote');
       submit.setAttribute('value', 'Vote for ' + nomineeName[i].innerText);
 		} else {
 			card[i].classList.remove('border-focus');
       hiddenRadio[i].checked = false;
+      // setTimeout(function(){ voteButton[i].classList.add('hide-vote'); }, 1000);
+      voteButton[i].classList.add('hide-vote');
       console.log('else being activated for item ' + (i + 1));
       count ++;
       if (count > card.length - 1) {
+        voteButton[i].classList.add('hide-vote');
         submit.classList.add('hidden-button');
       } else {
         submit.classList.remove('hidden-button');
