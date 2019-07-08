1639416d41b13bcaecd1c655a6953b177cbe7510
Dace Howard <dace61@gmail.com>
2019-07-08 09:42:06 +0930

    Removed unused JS

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const hiddenRadio = document.querySelectorAll('input[type=radio]');
-const nominees = document.getElementById('nominees');
 const card = document.querySelectorAll('.nominee-card');
-const submit = document.getElementById('vote-now');
-const nomineeName = document.querySelectorAll('.card-text h2');
 const voteButton = document.querySelectorAll('.single-vote');
 const welcomeDisplay = document.getElementsByClassName('welcome-display')[0];
 const startButton = document.getElementsByClassName('start-button')[0];
 
 document.addEventListener('click', (e) => {
-  let count = 0;
 	for (i = 0; i < card.length; i++) {
 		if (e.target === card[i]) {
-      // console.log('This click works for nominee ' + (i+1));
 			hiddenRadio[i].checked = true;
       e.target.classList.add('border-focus');
       voteButton[i].classList.remove('hide-vote');
@@ -19,11 +14,6 @@ document.addEventListener('click', (e) => {
 			card[i].classList.remove('border-focus');
       hiddenRadio[i].checked = false;
       voteButton[i].classList.add('hide-vote');
-      // console.log('else being activated for item ' + (i + 1));
-      // count ++;
-      // if (count > card.length - 1) {
-      //   voteButton[i].classList.add('hide-vote');
-      // }
 		}
 	}
 });
