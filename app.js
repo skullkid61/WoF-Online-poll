54c58c7b9db09f89bc7ff9acfb4be8b6bc37efa5
Dace Howard <dace61@gmail.com>
2019-07-08 11:28:54 +0930

    Goodbye model added

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const hiddenRadio = document.querySelectorAll('input[type=radio]');
 const card = document.querySelectorAll('.nominee-card');
 const voteButton = document.querySelectorAll('.single-vote');
 const welcomeDisplay = document.getElementsByClassName('welcome-display')[0];
+const goodbyeDisplay = document.getElementsByClassName('goodbye-display')[0];
 const startButton = document.getElementsByClassName('start-button')[0];
+const form = document.getElementById('form');
 
 document.addEventListener('click', (e) => {
 	for (i = 0; i < card.length; i++) {
@@ -12,7 +14,7 @@ document.addEventListener('click', (e) => {
       voteButton[i].classList.remove('hide-vote');
 		} else {
 			card[i].classList.remove('border-focus');
-      hiddenRadio[i].checked = false;
+      // hiddenRadio[i].checked = false;
       voteButton[i].classList.add('hide-vote');
 		}
 	}
@@ -20,4 +22,12 @@ document.addEventListener('click', (e) => {
 
 startButton.addEventListener('click', (e) => {
   welcomeDisplay.classList.add('hide-display');
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  goodbyeDisplay.classList.remove('hide-display');
+  setTimeout( () => { 
+    form.submit()
+  }, 8000);
 });
